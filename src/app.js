61c3ccb9f5412ca422380e022c86b9e9ca785c9c
Em McDonald <mcdonald.emily@live.co.uk>
2019-09-23 01:02:06 +0100

    :sparkles: Hooked up validation with input state
    Outline the input boxes with red if they contain an invalid value.

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import { TextField, Typography } from '@material-ui/core';
 
 import ButtonContainer from './components/button-container';
+import { isValidState, isValidInstructions } from './utils/validation-utils';
 
 const MAX_ROVERS = 10;
 
@@ -18,29 +19,42 @@ const StyledRow = styled.div`
   }
 `;
 
-const RoverRow = ({ id }) => (
-  <StyledRow>
-    <Typography variant="h4">Rover {id}</Typography>
-    <TextField
-      label="Start state"
-      placeholder="e.g. 1 2 N"
-      helperText="Space-separated values: X Y BEARING"
-    />
-    <TextField
-      label="Instructions to rove"
-      placeholder="e.g. LMRLMR"
-      helperText="L for Left, R for Right, M for Move"
-    />
-    <TextField
-      label="End state"
-      value="3 4 S"
-      helperText="Space-separated values: X Y BEARING"
-      InputProps={{
-        readOnly: true
-      }}
-    />
-  </StyledRow>
-);
+const RoverRow = ({ id }) => {
+  const [isStartStateStringValid, setIsStartStateStringValid] = useState(true);
+  const [isInstructionsStringValid, setIsInstructionsStringValid] = useState(
+    true
+  );
+
+  return (
+    <StyledRow>
+      <Typography variant="h4">Rover {id}</Typography>
+      <TextField
+        label="Start state"
+        placeholder="e.g. 1 2 N"
+        helperText="Space-separated values: X Y BEARING"
+        error={!isStartStateStringValid}
+        onChange={e => setIsStartStateStringValid(isValidState(e.target.value))}
+      />
+      <TextField
+        label="Instructions to rove"
+        placeholder="e.g. LMRLMR"
+        helperText="L for Left, R for Right, M for Move"
+        error={!isInstructionsStringValid}
+        onChange={e =>
+          setIsInstructionsStringValid(isValidInstructions(e.target.value))
+        }
+      />
+      <TextField
+        label="End state"
+        value="3 4 S"
+        helperText="Space-separated values: X Y BEARING"
+        InputProps={{
+          readOnly: true
+        }}
+      />
+    </StyledRow>
+  );
+};
 
 export const removeRover = rovers => [...rovers.slice(0, rovers.length - 1)];
 
