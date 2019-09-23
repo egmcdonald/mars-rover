8b3c426a6b5fea59a5fa4bbc57519b63dec4827f
Em McDonald <mcdonald.emily@live.co.uk>
2019-09-23 17:38:59 +0100

    :sparkles: Added functionality to move function

--- src/utils/rover-utils.js
+++ src/utils/rover-utils.js
@@ -1,4 +1,29 @@
-export const move = () => ({});
+const DIRECTIONS = {
+  N: {
+    x: 0,
+    y: 1
+  },
+  E: {
+    x: 1,
+    y: 0
+  },
+  S: {
+    x: 0,
+    y: -1
+  },
+  W: {
+    x: -1,
+    y: 0
+  }
+};
+
+export function move({ bearing, x, y }) {
+  const { x: xToMove, y: yToMove } = DIRECTIONS[bearing];
+  return {
+    x: x + xToMove,
+    y: y + yToMove
+  };
+}
 
 export const spin = () => '';
 
